0fb3a5127bcc0075a843f6f49439d6f12c8a1103
Brian T <sourboatlife@gmail.com>
2016-11-27 15:06:40 -0700

    first try making some ui models

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,17 +1,13 @@
-import java.sql.*;
+import com.sourboatlife.ui.models.Episode;
+import com.sourboatlife.ui.models.Show;
+import spark.ModelAndView;
+import spark.template.freemarker.FreeMarkerEngine;
+
+import java.util.Arrays;
 import java.util.HashMap;
-import java.util.ArrayList;
 import java.util.Map;
 
-import java.net.URI;
-import java.net.URISyntaxException;
-
 import static spark.Spark.*;
-import spark.template.freemarker.FreeMarkerEngine;
-import spark.ModelAndView;
-import static spark.Spark.get;
-
-import com.heroku.sdk.jdbc.DatabaseUrl;
 
 public class Main {
 
@@ -20,15 +16,17 @@ public class Main {
     port(Integer.valueOf(System.getenv("PORT")));
     staticFileLocation("/public");
 
-    get("/hello", (req, res) -> "Hello World");
-
     get("/", (request, response) -> {
             Map<String, Object> attributes = new HashMap<>();
-            attributes.put("message", "Hello World!");
+            attributes.put("show", new Show(Arrays.asList(new Episode.Builder()
+                    .withTitle("Test Data!")
+                    .withLink("http://www.twitch.tv/sourboatlife")
+                    .build())));
 
             return new ModelAndView(attributes, "index.ftl");
         }, new FreeMarkerEngine());
 
+    /*
     get("/db", (req, res) -> {
       Connection connection = null;
       Map<String, Object> attributes = new HashMap<>();
@@ -54,7 +52,7 @@ public class Main {
         if (connection != null) try{connection.close();} catch(SQLException e){}
       }
     }, new FreeMarkerEngine());
-
+    */
   }
 
 }
