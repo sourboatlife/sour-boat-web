5be6dd7b2110e4bb1a8059275e3ea5f8fe61c357
Brian <sourboatlife@gmail.com>
2016-12-11 13:55:58 -0700

    Added JSON source file and retriever to get stubbed and non-stubbed data. Played with CSS a little

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,62 +1,26 @@
-import com.sourboatlife.ui.models.Episode;
+import com.sourboatlife.dataaccess.JsonShowRetriever;
 import com.sourboatlife.ui.models.Show;
 import spark.ModelAndView;
 import spark.template.freemarker.FreeMarkerEngine;
 
-import java.time.ZonedDateTime;
-import java.util.Arrays;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import static spark.Spark.*;
 
 public class Main {
 
-  public static void main(String[] args) {
-    port(Integer.valueOf(System.getenv("PORT")));
-    staticFileLocation("/public");
+    public static void main(String[] args) {
+        port(5000);
+        staticFileLocation("/public");
 
-    get("/", (request, response) -> {
+        final List<Show> shows = new JsonShowRetriever().getShows();
+
+        get("/", (request, response) -> {
             Map<String, Object> attributes = new HashMap<>();
-            attributes.put("show", new Show(Arrays.asList(new Episode.Builder()
-                    .withTitle("Title of this week's Show")
-                    .withDescription("This is a long description of what it is we did during this show. This is a long description of what it is we did during this show. This is a long description of what it is we did during this show. This is a long description of what it is we did during this show. This is a long description of what it is we did during this show. This is a long description of what it is we did during this show. This is a long description of what it is we did during this show. This is a long description of what it is we did during this show. This is a long description of what it is we did during this show. This is a long description of what it is we did during this show. This is a long description of what it is we did during this show.")
-                    .withLink("https://www.youtube.com/embed/AZc0QiGdzl8")
-                    .withLink("https://www.youtube.com/embed/t5IYkoQ-2hY")
-                    .withTag("Programming").withTag("UI Frameworks")
-                    .withOriginalAirDate(ZonedDateTime.now())
-                    .build())));
-
+            attributes.put("shows", shows);
             return new ModelAndView(attributes, "index.ftl");
         }, new FreeMarkerEngine());
-
-    /*
-    get("/db", (req, res) -> {
-      Connection connection = null;
-      Map<String, Object> attributes = new HashMap<>();
-      try {
-        connection = DatabaseUrl.extract().getConnection();
-
-        Statement stmt = connection.createStatement();
-        stmt.executeUpdate("CREATE TABLE IF NOT EXISTS ticks (tick timestamp)");
-        stmt.executeUpdate("INSERT INTO ticks VALUES (now())");
-        ResultSet rs = stmt.executeQuery("SELECT tick FROM ticks");
-
-        ArrayList<String> output = new ArrayList<String>();
-        while (rs.next()) {
-          output.add( "Read from DB: " + rs.getTimestamp("tick"));
-        }
-
-        attributes.put("results", output);
-        return new ModelAndView(attributes, "db.ftl");
-      } catch (Exception e) {
-        attributes.put("message", "There was an error: " + e);
-        return new ModelAndView(attributes, "error.ftl");
-      } finally {
-        if (connection != null) try{connection.close();} catch(SQLException e){}
-      }
-    }, new FreeMarkerEngine());
-    */
-  }
-
+    }
 }
--- src/main/resources/spark/template/freemarker/index.ftl
+++ src/main/resources/spark/template/freemarker/index.ftl
@@ -6,22 +6,25 @@
 
 <body>
     <main>
-        <h1>Sour Boat Life</h1>
-        <#list show.episodes as episode>
-            <article class="episode">
-                <details>
-                    <summary>
-                        <h2>${episode.originalAirDate}: ${episode.title?html}</h2>
-                    </summary>
-                    <p>${episode.description}</p>
-                    <ul class="parts">
-                        <#list episode.links as url>
-                            <li><iframe width="560" height="315" src="${url}" frameborder="0" allowfullscreen></iframe></li>
-                        </#list>
-                    </ul>
-                    <footer><#list episode.tags as tag><span>${tag}</span></#list></footer>
-                </details>
-            </article>
+        <#list shows as show>
+            <h1>${show.title}</h1>
+            <#list show.episodes as episode>
+                <article class="episode">
+                    <details <#if episode?counter == 1>open</#if>>
+                        <summary>
+                            <h2>${episode.title?html}</h2>
+                            <span class="airdate">${episode.originalAirDate}</span>
+                            <p>${episode.description}</p>
+                        </summary>
+                        <ul class="parts"
+                            <#list episode.links as url>
+                                <li><iframe width="560" height="315" src="${url}" frameborder="0" allowfullscreen></iframe></li>
+                            </#list>
+                        </ul>
+                        <footer><#list episode.tags as tag><span>${tag}</span></#list></footer>
+                    </details>
+                </article>
+            </#list>
         </#list>
     </main>
 </body>
